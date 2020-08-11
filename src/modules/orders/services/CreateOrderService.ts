a67d87904c6fef0ed148a291f4801eb66c6c9f75
Tiala Rocha <tialarocha@gmail.com>
2020-08-11 13:22:41 -0300

    first commit

--- src/modules/orders/services/CreateOrderService.ts
+++ src/modules/orders/services/CreateOrderService.ts
@@ -20,13 +20,57 @@ interface IRequest {
 @injectable()
 class CreateOrderService {
   constructor(
+    @inject('OrdersRepository')
     private ordersRepository: IOrdersRepository,
+
+    @inject('ProductsRepository')
     private productsRepository: IProductsRepository,
+
+    @inject('CustomersRepository')
     private customersRepository: ICustomersRepository,
   ) {}
 
   public async execute({ customer_id, products }: IRequest): Promise<Order> {
-    // TODO
+    const customer = await this.customersRepository.findById(customer_id);
+
+    if (!customer) {
+      throw new AppError('Customer not found.');
+    }
+
+    const foundProductsIds = products.map(product => ({
+      id: product.id,
+    }));
+
+    const productsForGetPrices = await this.productsRepository.findAllById(
+      foundProductsIds,
+    );
+
+    if (!productsForGetPrices) {
+      throw new AppError('Products not found.');
+    }
+
+    const orderProducts = products.map(requestProduct => {
+      const productBD = productsForGetPrices.find(
+        prodBD => prodBD.id === requestProduct.id,
+      );
+
+      if (!productBD) {
+        throw new AppError('Invalid Product.');
+      }
+
+      return {
+        product_id: requestProduct.id,
+        price: productBD.price,
+        quantity: requestProduct.quantity,
+      };
+    });
+
+    const order = await this.ordersRepository.create({
+      customer,
+      products: orderProducts,
+    });
+
+    return order;
   }
 }
 
